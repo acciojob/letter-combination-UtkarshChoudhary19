250504a4658f97f33e6ba61416bc809ce1c9e066
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-01 18:08:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function letterCombinations(input_digit) {
+// function letterCombinations(input_digit) {
   //Complete the function
+	var letterCombinations = function(digits) {
+    const result = [];
+    if (!digits) {
+        return result;
+    }
+    const mapping = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+    const combination = new Array(digits.length).fill('');
+    backtrack(result, mapping, combination, digits, 0);
+    return result;
+};
+
+function backtrack(result, mapping, combination, digits, index) {
+    if (index === digits.length) {
+        result.push(combination.join(''));
+    } else {
+        const letters = mapping[digits.charAt(index) - '0'];
+        for (const letter of letters) {
+            combination[index] = letter;
+            backtrack(result, mapping, combination, digits, index + 1);
+        }
+    }
 }
+// }
 
 module.exports = letterCombinations;
